8e16aec8f32fbfdb29dcdb68ec89ec2c6d3717c5
Shlomi Yahbes <syahbes@gmail.com>
2024-06-14 21:04:09 +0300

    init

--- App.tsx
+++ App.tsx
@@ -1,7 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import * as FileSystem from "expo-file-system";
+import { Asset } from "expo-asset";
+
+const loadDatabase = async () => {
+  const dbName = "mySQLiteDB.db";
+  const dbAsset = require("./assets/mySQLiteDB.db");
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  try {
+    const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+    if (!fileInfo.exists) {
+      await FileSystem.makeDirectoryAsync(`${FileSystem.documentDirectory}SQLite`, { intermediates: true });
+      await FileSystem.downloadAsync(dbUri, dbFilePath);
+    }
+  } catch (error) {
+    console.error("Error loading database: ", error);
+  }
+};
+
 
 export default function App() {
+  const [dbLoaded, setDbLoaded] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    loadDatabase()
+      .then(() => {
+        setDbLoaded(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!dbLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +54,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
