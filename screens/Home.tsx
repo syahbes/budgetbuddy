46931da04b6b0a70b4f571d112d36165833848ce
Shlomi Yahbes <syahbes@gmail.com>
2024-06-15 19:25:35 +0300

    almost done

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text, View, TextStyle, StyleSheet } from "react-native";
 import { Category, Transaction, TransactionsByMonth } from "../types";
 import { useSQLiteContext } from "expo-sqlite/next";
 import TransactionsList from "../components/TransactionsList";
 import Card from "../components/ui/Card";
+import AddTransaction from "../components/AddTransaction";
 
 export default function Home() {
   const [categories, setCategories] = React.useState<Category[]>([]);
@@ -33,6 +34,28 @@ export default function Home() {
         "SELECT * FROM Categories"
       );
       setCategories(categoriesResult);
+      //Todo: move to helper function
+      const now = new Date();
+      const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+      const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+      endOfMonth.setMilliseconds(endOfMonth.getMilliseconds() - 1);
+
+      // convert to Unix timestamp
+      const startOfMonthTimestamp = Math.floor(startOfMonth.getTime() / 1000);
+      const endOfMonthTimestamp = Math.floor(endOfMonth.getTime() / 1000);
+
+      const transactionsByMonth = await db.getAllAsync<TransactionsByMonth>(
+        `
+        SELECT
+          COALESCE(SUM(CASE WHEN type = "Expense" THEN amount ELSE 0 END),0) AS totalExpenses,
+          COALESCE(SUM(CASE WHEN type = "Income" THEN amount ELSE 0 END),0) AS totalIncome
+        FROM Transactions
+        WHERE date >= ? AND date <= ?
+        `,
+        [startOfMonthTimestamp, endOfMonthTimestamp]
+      );
+
+      setTransactionsByMonth(transactionsByMonth[0]);
     } catch (error) {
       console.log(error);
     }
@@ -48,9 +71,26 @@ export default function Home() {
       console.log(error);
     }
   }
-
+  async function insertTransaction(transaction: Transaction) {
+    db.withTransactionAsync(async () => {
+      await db.runAsync(
+        `
+        INSERT INTO Transactions (category_id, amount, date, description, type) VALUES (?, ?, ?, ?, ?);
+      `,
+        [
+          transaction.category_id,
+          transaction.amount,
+          transaction.date,
+          transaction.description,
+          transaction.type,
+        ]
+      );
+      await getData();
+    });
+  }
   return (
     <ScrollView contentContainerStyle={{ padding: 15, paddingVertical: 10 }}>
+      <AddTransaction insertTransaction={insertTransaction} />
       <TransactionSummary
         totalIncome={transactionsByMonth.totalIncome}
         totalExpenses={transactionsByMonth.totalExpenses}
@@ -73,9 +113,57 @@ function TransactionSummary({
     month: "long",
     year: "numeric",
   });
+
+  // Function to determine the style based on the value (positive or negative)
+  const getMoneyTextStyle = (value: number): TextStyle => ({
+    fontWeight: "bold",
+    color: value < 0 ? "#ff4500" : "#2e8b57", // Red for negative, custom green for positive
+  });
+
+  // Helper function to format monetary values
+  const formatMoney = (value: number) => {
+    const absValue = Math.abs(value).toFixed(2);
+    return `${value < 0 ? "-" : ""}$${absValue}`;
+  };
+
   return (
-    <Card>
-      <Text>Summary for {readablePeriod}</Text>
+    <Card style={styles.container}>
+      <Text style={styles.periodTitle}>Summary for {readablePeriod}</Text>
+      <Text style={styles.summaryText}>
+        Income:{" "}
+        <Text style={getMoneyTextStyle(totalIncome)}>
+          {formatMoney(totalIncome)}
+        </Text>
+      </Text>
+      <Text style={styles.summaryText}>
+        Total Expenses:{" "}
+        <Text style={getMoneyTextStyle(totalExpenses)}>
+          {formatMoney(totalExpenses)}
+        </Text>
+      </Text>
+      <Text style={styles.summaryText}>
+        Savings:{" "}
+        <Text style={getMoneyTextStyle(savings)}>{formatMoney(savings)}</Text>
+      </Text>
     </Card>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 15,
+    paddingBottom: 7,
+    // Add other container styles as necessary
+  },
+  periodTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 15,
+  },
+  summaryText: {
+    fontSize: 18,
+    color: "#333",
+    marginBottom: 10,
+  },
+});
